d4517be2263aae05ccbca84057359cb583511483
ajozefowicz <ginger4u@op.pl>
2022-02-27 17:52:46 +0100

    implementing the interface - added CategoryRepository

---
+++ CandyShop/CandyShop/Models/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> GettAllCategories => new List<Category>
+        {
+            new Category{CateogryId=1, CategoryName="Hard Candy", CategoryDescription="Super hard candy"},
+            new Category{CateogryId=2, CategoryName="Chocolate Candy", CategoryDescription="very chocolate candy"},
+            new Category{CateogryId=3, CategoryName="Fruit Candy", CategoryDescription="Sour fruit candy"},
+        };
+    }
+}
