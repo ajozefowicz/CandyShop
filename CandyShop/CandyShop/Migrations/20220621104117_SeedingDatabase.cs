d7d4e40844aeb4e42bf83185b31517b346a059a5
ajozefowicz <ginger4u@op.pl>
2022-06-21 12:44:06 +0200

    seeded database

---
+++ CandyShop/CandyShop/Migrations/20220621104117_SeedingDatabase.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandyShop.Migrations
+{
+    public partial class SeedingDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CateogryId", "CategoryDescription", "CategoryName" },
+                values: new object[] { 1, "Super hard candy", "Hard Candy" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CateogryId", "CategoryDescription", "CategoryName" },
+                values: new object[] { 2, "very chocolate candy", "Chocolate Candy" });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CateogryId", "CategoryDescription", "CategoryName" },
+                values: new object[] { 3, "Sour fruit candy", "Fruit Candy" });
+
+            migrationBuilder.InsertData(
+                table: "Candies",
+                columns: new[] { "CandyId", "CategoryId", "Description", "ImageThumbnailUrl", "ImageUrl", "IsInStock", "IsOnSale", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis", "https://cdn.pixabay.com/photo/2012/12/11/21/28/variation-69470__340.jpg", "https://cdn.pixabay.com/photo/2012/12/11/21/28/variation-69470__340.jpg", true, false, "Asorted Hard Candy", 2.22m },
+                    { 2, 1, "Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis", "https://cdn.pixabay.com/photo/2016/10/09/18/06/candy-corn-1726481_960_720.jpg", "https://cdn.pixabay.com/photo/2016/10/09/18/06/candy-corn-1726481_960_720.jpg", true, true, "Super Hard Candy", 4.22m },
+                    { 3, 2, "Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis", "https://cdn.pixabay.com/photo/2014/11/30/10/48/chocolate-551424__340.jpg", "https://cdn.pixabay.com/photo/2014/11/30/10/48/chocolate-551424__340.jpg", true, true, "Super Chocolate Candy", 3.33m },
+                    { 4, 2, "Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis", "https://cdn.pixabay.com/photo/2017/04/12/16/56/chocolates-2224998__340.jpg", "https://cdn.pixabay.com/photo/2017/04/12/16/56/chocolates-2224998__340.jpg", true, true, "Dark Chocolate Candy", 5.55m },
+                    { 5, 3, "Lorem ipsum dolor sit amet enim. Etiam ullamcorper. Suspendisse a pellentesque dui, non felis", "https://cdn.pixabay.com/photo/2018/06/19/23/30/sugar-3485447__340.jpg", "https://cdn.pixabay.com/photo/2018/06/19/23/30/sugar-3485447__340.jpg", true, true, "Mango Candy", 5.55m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Candies",
+                keyColumn: "CandyId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CateogryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CateogryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CateogryId",
+                keyValue: 3);
+        }
+    }
+}
--- CandyShop/CandyShop/Models/AppDbContext.cs
+++ CandyShop/CandyShop/Models/AppDbContext.cs
@@ -16,5 +16,78 @@ namespace CandyShop.Models
 
         public DbSet<Candy> Candies { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(new Category { CateogryId = 1, CategoryName = "Hard Candy", CategoryDescription = "Super hard candy" });
+            modelBuilder.Entity<Category>().HasData(new Category { CateogryId = 2, CategoryName = "Chocolate Candy", CategoryDescription = "very chocolate candy" });
+            modelBuilder.Entity<Category>().HasData(new Category { CateogryId = 3, CategoryName = "Fruit Candy", CategoryDescription = "Sour fruit candy" });
+            
+            modelBuilder.Entity<Candy>().HasData(new Candy {
+                CandyId = 1,
+                Name = "Asorted Hard Candy",
+                Price = 2.22M,
+                Description = "Lorem ipsum dolor sit amet enim. " +
+                "Etiam ullamcorper. Suspendisse a pellentesque dui, non felis",
+                CategoryId = 1,
+                ImageUrl = "https://cdn.pixabay.com/photo/2012/12/11/21/28/variation-69470__340.jpg",
+                IsInStock = true,
+                IsOnSale = false,
+                ImageThumbnailUrl = "https://cdn.pixabay.com/photo/2012/12/11/21/28/variation-69470__340.jpg"
+            });
+            modelBuilder.Entity<Candy>().HasData(new Candy {
+                CandyId = 2,
+                Name = "Super Hard Candy",
+                Price = 4.22M,
+                Description = "Lorem ipsum dolor sit amet enim. " +
+                "Etiam ullamcorper. Suspendisse a pellentesque dui, non felis",
+                CategoryId = 1,
+                ImageUrl = "https://cdn.pixabay.com/photo/2016/10/09/18/06/candy-corn-1726481_960_720.jpg",
+                IsInStock = true,
+                IsOnSale = true,
+                ImageThumbnailUrl = "https://cdn.pixabay.com/photo/2016/10/09/18/06/candy-corn-1726481_960_720.jpg"
+            });
+            modelBuilder.Entity<Candy>().HasData(new Candy {
+                CandyId = 3,
+                Name = "Super Chocolate Candy",
+                Price = 3.33M,
+                Description = "Lorem ipsum dolor sit amet enim. " +
+                "Etiam ullamcorper. Suspendisse a pellentesque dui, non felis",
+                CategoryId = 2,
+                ImageUrl = "https://cdn.pixabay.com/photo/2014/11/30/10/48/chocolate-551424__340.jpg",
+                IsInStock = true,
+                IsOnSale = true,
+                ImageThumbnailUrl = "https://cdn.pixabay.com/photo/2014/11/30/10/48/chocolate-551424__340.jpg"
+            });
+            modelBuilder.Entity<Candy>().HasData(new Candy {
+                CandyId = 4,
+                Name = "Dark Chocolate Candy",
+                Price = 5.55M,
+                Description = "Lorem ipsum dolor sit amet enim. " +
+                "Etiam ullamcorper. Suspendisse a pellentesque dui, non felis",
+                CategoryId = 2,
+                ImageUrl = "https://cdn.pixabay.com/photo/2017/04/12/16/56/chocolates-2224998__340.jpg",
+                IsInStock = true,
+                IsOnSale = true,
+                ImageThumbnailUrl = "https://cdn.pixabay.com/photo/2017/04/12/16/56/chocolates-2224998__340.jpg"
+            });
+
+             modelBuilder.Entity<Candy>().HasData(new Candy {
+                 CandyId = 5,
+                 Name = "Mango Candy",
+                 Price = 5.55M,
+                 Description = "Lorem ipsum dolor sit amet enim. " +
+                "Etiam ullamcorper. Suspendisse a pellentesque dui, non felis",
+                 CategoryId = 3,
+                 ImageUrl = "https://cdn.pixabay.com/photo/2018/06/19/23/30/sugar-3485447__340.jpg",
+                 IsInStock = true,
+                 IsOnSale = true,
+                 ImageThumbnailUrl = "https://cdn.pixabay.com/photo/2018/06/19/23/30/sugar-3485447__340.jpg"
+             });
+
+
+        }
     }
 }
