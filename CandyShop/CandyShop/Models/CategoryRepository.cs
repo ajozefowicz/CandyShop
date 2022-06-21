0592d93a12f9da2cb16483acf634f8ed91a897c7
ajozefowicz <ginger4u@op.pl>
2022-06-21 11:57:17 +0200

    CategoryRepo has been modified

--- CandyShop/CandyShop/Models/CategoryRepository.cs
+++ CandyShop/CandyShop/Models/CategoryRepository.cs
@@ -7,11 +7,14 @@ namespace CandyShop.Models
 {
     public class CategoryRepository : ICategoryRepository
     {
-        public IEnumerable<Category> GetAllCategories => new List<Category>
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
         {
-            new Category{CateogryId=1, CategoryName="Hard Candy", CategoryDescription="Super hard candy"},
-            new Category{CateogryId=2, CategoryName="Chocolate Candy", CategoryDescription="very chocolate candy"},
-            new Category{CateogryId=3, CategoryName="Fruit Candy", CategoryDescription="Sour fruit candy"},
-        };
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Category> GetAllCategories => _appDbContext.Categories;
+
     }
 }
