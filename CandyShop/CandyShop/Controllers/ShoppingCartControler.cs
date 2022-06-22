8785977c31695935685819e379f51a4bf21c97d5
ajozefowicz <ginger4u@op.pl>
2022-06-22 14:50:59 +0200

    added ShoppingCartControler and ViewResult Index

---
+++ CandyShop/CandyShop/Controllers/ShoppingCartControler.cs
@@ -0,0 +1,37 @@
+﻿using CandyShop.Models;
+using CandyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Controllers
+{
+    public class ShoppingCartControler : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartControler(ICandyRepository candyRepository, ShoppingCart shoppingCart)
+        {
+            _candyRepository = candyRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            _shoppingCart.ShoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotalPrice()
+            };
+
+            return View(shoppingCartViewModel);
+    }
+
+
+}
+}
