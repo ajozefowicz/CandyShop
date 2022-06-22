b6fce4a1add5a580f6f20277177987f8c6ebf650
ajozefowicz <ginger4u@op.pl>
2022-06-22 13:46:36 +0200

    added method AddToCart

--- CandyShop/CandyShop/Models/ShoppingCart.cs
+++ CandyShop/CandyShop/Models/ShoppingCart.cs
@@ -21,7 +21,7 @@ namespace CandyShop.Models
         public static ShoppingCart GetCart(IServiceProvider services)
         {
             ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
-           
+
             var context = services.GetService<AppDbContext>();
             string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
             session.SetString("CartId", cartId);
@@ -29,5 +29,27 @@ namespace CandyShop.Models
             return new ShoppingCart(context) { ShoppingCartId = cartId };
         }
 
+        public void AddToCart(Candy candy, int amount)
+        {
+            var shoppingCartItem = _appDbContext.ShoppingCartItems.SingleOrDefault
+                (s => s.Candy.CandyId == candy.CandyId && s.ShoppingCardId == ShoppingCartId);
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCardId = ShoppingCartId,
+                    Candy = candy,
+                    Amount = amount
+                };
+
+                _appDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+
+            _appDbContext.SaveChanges();
+        }
     }
 }
