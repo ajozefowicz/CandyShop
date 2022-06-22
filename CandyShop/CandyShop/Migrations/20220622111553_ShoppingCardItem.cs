f0edbf97f4a1cf6c998e687006c1b2e99f31c8f1
ajozefowicz <ginger4u@op.pl>
2022-06-22 13:17:12 +0200

    added ShoppingCardItem

---
+++ CandyShop/CandyShop/Migrations/20220622111553_ShoppingCardItem.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandyShop.Migrations
+{
+    public partial class ShoppingCardItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShoppingCartItems",
+                columns: table => new
+                {
+                    ShoppingCardItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShoppingCardId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CandyId = table.Column<int>(type: "int", nullable: true),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCartItems", x => x.ShoppingCardItemId);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCartItems_Candies_CandyId",
+                        column: x => x.CandyId,
+                        principalTable: "Candies",
+                        principalColumn: "CandyId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItems_CandyId",
+                table: "ShoppingCartItems",
+                column: "CandyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShoppingCartItems");
+        }
+    }
+}
---
+++ CandyShop/CandyShop/Models/ShoppingCartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int ShoppingCardItemId { get; set; }
+        public string ShoppingCardId { get; set; }
+        public Candy Candy { get; set; }
+        public int Amount { get; set; }
+    }
+}
