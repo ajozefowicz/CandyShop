703c43d167cf52e8fb3b02445c91dbcb12a4df4d
ajozefowicz <ginger4u@op.pl>
2022-06-27 23:01:42 +0200

    added Category Menu View Component - with error to repear

---
+++ CandyShop/CandyShop/Components/CategoryMenu.cs
@@ -0,0 +1,31 @@
+﻿using CandyShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Components
+{
+   // [ViewComponent(Name = "Dupa")]
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        //this is the view component so we need Invoke method
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.GetAllCategories.OrderBy(c => c.CategoryName);
+
+            return View(categories);
+        }
+
+
+    }
+}
